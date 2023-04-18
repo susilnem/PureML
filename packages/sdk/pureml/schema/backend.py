3f104a333c07da3c67f080fc1f432136ca030df0
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2023-04-19 01:52:11 +0530

    Bug fix for backend_url verification. Typer functions are defaulted to typer.option data type. type check is missing in backend and frontend url

--- packages/sdk/pureml/schema/backend.py
+++ packages/sdk/pureml/schema/backend.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, root_validator
 from .singleton import Singleton_BaseModel
 import typing
 import os
+import typer
 
 from pathlib import Path
 from pureml.cli.puremlconfig import PureMLConfigYML
@@ -15,28 +16,46 @@ else:
 
 
 def get_backend_base_url(backend_url: str = None):
-    if backend_url is not None and backend_url != "":
+    if (
+        backend_url is not None
+        and backend_url != ""
+        and type(backend_url) == typer.Option
+    ):
         # override backend url (command specific option)
         return backend_url
     if puremlconfig is not None:
         # user configured backend url (self-hosted or custom pureml backend instance)
-        backend_url = puremlconfig.data["backend_url"] if "backend_url" in puremlconfig.data else "https://pureml-development.up.railway.app/api/"
+        backend_url = (
+            puremlconfig.data["backend_url"]
+            if "backend_url" in puremlconfig.data
+            else "https://pureml-development.up.railway.app/api/"
+        )
     else:
         # default backend url (production cloud backend)
         backend_url = "https://pureml-development.up.railway.app/api/"
     return backend_url
 
+
 def get_frontend_base_url(frontend_url: str = None):
-    if frontend_url is not None and frontend_url != "":
+    if (
+        frontend_url is not None
+        and frontend_url != ""
+        and type(frontend_url) == typer.Option
+    ):
         # override frontend url (command specific option)
         return frontend_url
     if puremlconfig is not None:
         # user configured frontend url (self-hosted or custom pureml frontend instance)
-        frontend_url = puremlconfig.data["frontend_url"] if "frontend_url" in puremlconfig.data else "https://pureml.com/"
+        frontend_url = (
+            puremlconfig.data["frontend_url"]
+            if "frontend_url" in puremlconfig.data
+            else "https://pureml.com/"
+        )
     else:
         # default frontend url (production cloud frontend)
         frontend_url = "https://pureml.com/"
     return frontend_url
+
 
 class BackendSchema(Singleton_BaseModel):
 
@@ -61,7 +80,7 @@ class BackendSchema(Singleton_BaseModel):
                 "bucket_name",
                 "public_url",
             ],
-        }
+        },
     }
 
     class Config:
